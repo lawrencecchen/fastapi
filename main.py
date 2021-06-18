5c524222f5ae5c7f0fddf93e701dd4cad074311b
lawrencecchen <md.lawrence.chen@gmail.com>
2021-06-17 22:14:43 -0700

    initial commit

---
+++ main.py
@@ -0,0 +1,13 @@
+from fastapi import FastAPI
+
+app = FastAPI()
+
+
+@app.get("/")
+async def root():
+    return {"message": "Hello, world!"}
+
+
+@app.get("/hello/{name}")
+async def hello(name: str):
+    return {"message": f"Hello, {name}!"}
